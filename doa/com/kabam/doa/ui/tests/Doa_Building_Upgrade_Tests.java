46ab23687c37ae854b2f82e328358713a5c4136d
Waylon Stipes <wstipes@watercooler-inc.com>
2013-02-21 16:38:30 -0800

    Updated

--- doa/com/kabam/doa/ui/tests/Doa_Building_Upgrade_Tests.java
+++ doa/com/kabam/doa/ui/tests/Doa_Building_Upgrade_Tests.java
@@ -4,7 +4,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-@Test(dependsOnGroups = { "Doa_Building_Construction" }, groups = { "Doa_Building_Upgrade" , "regression"})
+@Test(groups = { "Doa_Building_Upgrade" , "regression"})
 
 
 public class Doa_Building_Upgrade_Tests extends Doa_Test_BaseFixture {
@@ -13,7 +13,7 @@ public class Doa_Building_Upgrade_Tests extends Doa_Test_BaseFixture {
 	public void test_level_setup() {
 
 	}
-
+	
 	@AfterMethod
 	public void test_level_teardown() {
 		tearDown();
